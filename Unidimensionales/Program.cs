279b002f690e00750bb91c80c2e939ef4cf86e8c
Gerardo Chapa <gerardochapa1000@hotmail.com>
2018-09-10 18:46:15 -0500

    Corregido lectura y escritura de vectores, faltan operaciones

--- Unidimensionales/Program.cs
+++ Unidimensionales/Program.cs
@@ -6,31 +6,40 @@ namespace ArreglosUnidimensionales
     {
         static void Main()
         {
+            bool excep = false; 
+            do
+            {
+                Console.WriteLine("    ~CALCULADORA DE VECTORES~\n\nIngresa un vector de tres componentes.\nSepara cada uno con una coma sin espacio.");
+                Console.WriteLine("*****************\n**  EJEMPLOS   **\n**    1,3,5    **\n**   50,21,0   **\n*****************");
+                string entrada = Console.ReadLine();
+                try
+                {
+                    if (entrada.Contains(","))
+                    {
+                        Unidimensional v1 = Unidimensional.Leer(entrada);
+                        Console.WriteLine(v1.ToString());
+                    }
+                    else
+                    {
+                        throw new ApplicationException("Has ingresado de forma incorrecta los componentes de tu vector.");
+                    }
+                }
 
-            Console.WriteLine("    ~CALCULADORA DE VECTORES~\n\nIngresa un vector de tres componentes.\nSepara cada uno con una coma sin espacio.");
-            Console.WriteLine("*****************\n**  EJEMPLOS   **\n**    1,3,5    **\n**   50,21,0   **\n*****************");
-            string[] v1 = new string[3];
-            string entrada = Console.ReadLine();
-            try
-            {
-                if (entrada.Contains(";"))
+                catch (ApplicationException error)
                 {
-                    v1 = Unidimensional.Leer(entrada);
-                    Console.WriteLine(v1.ToString());
+                    Console.WriteLine("Error: " + error.Message);
+                    excep = true;
                 }
-                else
+
+                catch (FormatException error)
                 {
-                    throw new ApplicationException("Has ingresado de forma incorrecta los componentes de tu vector.");
+                    Console.WriteLine("Error: " + error.Message);
+                    excep = true;
                 }
-            }
-            catch(FormatException error)
-            {
-                Console.WriteLine("Error: "+error.Message);
-            }
-            catch(ApplicationException error)
-            {
-                Console.WriteLine("Error: " +error.Message);
-            }
+
+            } while (excep);
+
+            Console.ReadLine();
         }
     }
 }
--- Unidimensionales/Unidimensional.cs
+++ Unidimensionales/Unidimensional.cs
@@ -6,6 +6,11 @@ namespace ArreglosUnidimensionales
     {
         float[] matriz = new float[3];
 
+        public Unidimensional()
+        {
+
+        }
+
         public static Unidimensional Leer(string elementos)
         {
             string[] columnas = elementos.Split(','); //columnas es una matriz auxiliar
@@ -15,6 +20,17 @@ namespace ArreglosUnidimensionales
                 vector.matriz[i] = float.Parse(columnas[i]);
             }
             return vector;
+        }
+
+        public override string ToString()
+        {
+            string aux = "(";
+            for (int i = 0; i < 3; i++)
+            {
+                aux = aux + " " + matriz[i];
+            }
+            aux = aux + ")";
+            return aux;
         }
 
     }

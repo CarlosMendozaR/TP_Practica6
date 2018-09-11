b883fed3fb114181a67c69930b5d9aecfa694d90
Carlos <halomau@hotmail.com>
2018-09-10 19:37:17 -0500

    ya quedó

--- Unidimensionales/Program.cs
+++ Unidimensionales/Program.cs
@@ -14,7 +14,7 @@ namespace ArreglosUnidimensionales
             v1 = Unidimensional.Leer(dato);
             Console.WriteLine("Tu vector es: "+ v1.ToString()+"\n");
 
-            Console.WriteLine("Selecciona la opción que desees.\n0) Suma con otro vector\n1) Multiplicación por un escalar\n2) Multiplicación por otro vector");
+            Console.WriteLine("Selecciona la opción que desees.\n  0) Suma con otro vector\n  1) Multiplicación por un escalar\n  2) Multiplicación por otro vector\n");
             byte caso = byte.Parse(Console.ReadLine());
 
             switch (caso)

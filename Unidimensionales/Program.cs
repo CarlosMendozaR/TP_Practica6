002d8b152085ba1d12df0c4826f19ed29ee5b14c
Carlos <halomau@hotmail.com>
2018-09-10 19:32:13 -0500

    Creo que ya quedó

--- Unidimensionales/Program.cs
+++ Unidimensionales/Program.cs
@@ -6,40 +6,45 @@ namespace ArreglosUnidimensionales
     {
         static void Main()
         {
-            bool excep = false; 
-            do
+            Console.WriteLine("    ~CALCULADORA DE VECTORES~\n\nIngresa un vector de tres componentes.\nSepara cada uno con una coma sin espacio.");
+            Console.WriteLine("*****************\n**  EJEMPLOS   **\n**    1,3,5    **\n**   50,21,0   **\n*****************");
+            string dato = "";
+
+            Unidimensional v1;
+            v1 = Unidimensional.Leer(dato);
+            Console.WriteLine("Tu vector es: "+ v1.ToString()+"\n");
+
+            Console.WriteLine("Selecciona la opción que desees.\n0) Suma con otro vector\n1) Multiplicación por un escalar\n2) Multiplicación por otro vector");
+            byte caso = byte.Parse(Console.ReadLine());
+
+            switch (caso)
             {
-                Console.WriteLine("    ~CALCULADORA DE VECTORES~\n\nIngresa un vector de tres componentes.\nSepara cada uno con una coma sin espacio.");
-                Console.WriteLine("*****************\n**  EJEMPLOS   **\n**    1,3,5    **\n**   50,21,0   **\n*****************");
-                string entrada = Console.ReadLine();
-                try
-                {
-                    if (entrada.Contains(","))
-                    {
-                        Unidimensional v1 = Unidimensional.Leer(entrada);
-                        Console.WriteLine(v1.ToString());
-                    }
-                    else
-                    {
-                        throw new ApplicationException("Has ingresado de forma incorrecta los componentes de tu vector.");
-                    }
-                }
+                case 0:
+                    Console.WriteLine("Ingresa otro vector que quieres sumar.");
+                    Unidimensional v2 = Unidimensional.Leer(dato);
+                    Console.WriteLine("Tu otro vector es: "+v2.ToString()+"\n");
+                    Unidimensional v3 = v1 + v2;
+                    Console.WriteLine("RESULTADO");
+                    Console.WriteLine(v3.ToString());
+                    break;
 
-                catch (ApplicationException error)
-                {
-                    Console.WriteLine("Error: " + error.Message);
-                    excep = true;
-                }
+                case 1:
+                    Console.WriteLine("Ingresa el escalar por el que quieres multiplicar tu vector.");
+                    int escalar = int.Parse(Console.ReadLine());
+                    Unidimensional res1 = new Unidimensional();
+                    Console.WriteLine("RESULTADO");
+                    res1.M_Escalar(v1, escalar);
+                    break;
 
-                catch (FormatException error)
-                {
-                    Console.WriteLine("Error: " + error.Message);
-                    excep = true;
-                }
-
-            } while (excep);
-
-            Console.ReadLine();
+                case 2:
+                    Console.WriteLine("Ingresa otro vector por el que quieres multiplicar.");
+                    Unidimensional v4 = Unidimensional.Leer(dato);
+                    Console.WriteLine("Tu otro vector es: "+v4.ToString()+"\n");
+                    Unidimensional res2 = new Unidimensional();
+                    Console.WriteLine("RESULTADO");
+                    res2.M_Vector(v1, v4);
+                    break;
+            }
         }
     }
 }
--- Unidimensionales/Unidimensional.cs
+++ Unidimensionales/Unidimensional.cs
@@ -4,22 +4,41 @@ namespace ArreglosUnidimensionales
 {
     class Unidimensional
     {
-        float[] matriz = new float[3];
+        double[] matriz = new double[3];
 
-        public Unidimensional()
+        public static Unidimensional Leer(string entrada)
         {
-
-        }
-
-        public static Unidimensional Leer(string elementos)
-        {
-            string[] columnas = elementos.Split(','); //columnas es una matriz auxiliar
             Unidimensional vector = new Unidimensional();
-            for (int i = 0; i < 3; i++)
+            entrada = Console.ReadLine();
+
+            try
             {
-                vector.matriz[i] = float.Parse(columnas[i]);
+                if (entrada.Contains(","))
+                {
+                    for (int i = 0; i < vector.matriz.Length; i++)
+                    {
+                        vector.matriz[i] = Convert.ToDouble(Convert.ToString(entrada[i * 2]));
+                        //entrada[i*2] es un char[], entonces convierto primero a string y después a int
+                    }
+                }
+                else
+                {
+                    throw new ApplicationException("Has ingresado de forma incorrecta los componentes de tu vector.");
+                }
             }
+            catch (FormatException error)
+            {
+                Console.WriteLine("Error: " + error.Message);
+
+            }
+            catch (ApplicationException error)
+            {
+                Console.WriteLine("Error: " + error.Message);
+            }
+
             return vector;
+
+
         }
 
         public override string ToString()
@@ -29,9 +48,39 @@ namespace ArreglosUnidimensionales
             {
                 aux = aux + " " + matriz[i];
             }
-            aux = aux + ")";
+            aux = aux + " )";
             return aux;
         }
 
+
+        public static Unidimensional operator +(Unidimensional v1, Unidimensional v2)
+        {
+            Unidimensional v3 = new Unidimensional();
+            for (int i = 0; i < v1.matriz.Length; i++)
+            {
+                v3.matriz[i] = v1.matriz[i] + v2.matriz[i];
+            }
+            return v3;
+        }
+
+        public void M_Escalar(Unidimensional x, int cte)
+        {
+            Unidimensional z = new Unidimensional();
+            for (int c = 0; c < x.matriz.Length; c++)
+            {
+                z.matriz[c] = cte*x.matriz[c];
+            }
+            Console.WriteLine(z.ToString());
+        }
+
+        public void M_Vector(Unidimensional x, Unidimensional y)
+        {
+            Unidimensional z = new Unidimensional();
+            z.matriz[0] =(x.matriz[1]*y.matriz[2])-(y.matriz[1]*x.matriz[2]);
+            z.matriz[1] =(y.matriz[0]*x.matriz[2])-(x.matriz[0]*y.matriz[2]);
+            z.matriz[2] =(x.matriz[0]*y.matriz[1])-(y.matriz[0]*x.matriz[1]);
+
+            Console.WriteLine(z.ToString());
+        }
     }
 }
